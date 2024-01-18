64c57a3c5962e86fde9eeb6f374fc71968c6ff86
nikhilgithub911 <eee.190610446@silicon.ac.in>
2024-01-18 18:25:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-
+import Form from "./components/Form"
+import Aotable from "./components/Aotable"
+// import AgmDash from './components/AgmDash';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
+      {/* <Form/> */}
+      {/* <AgmDash/> */}
+      <Aotable/>
     </div>
   );
 }
 
 export default App;
+
+
+// aotable
