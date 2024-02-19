5ab6c5cd5d0ef6b70cdeb0e25419047a1503cbae
HARSHITA77105 <21mca22@imit.ac.in>
2024-02-19 10:48:21 +0530

    updated code

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -209,7 +209,7 @@ function Navbar({
                 {/* </IconButton> */}
               </Tooltip>
               <Menu
-                sx={{ mt: "45px" }}
+                sx={{ mt: "29px", marginLeft: "10px" }}
                 id="menu-appbar"
                 anchorEl={anchorElUser}
                 anchorOrigin={{
@@ -220,6 +220,32 @@ function Navbar({
                 transformOrigin={{
                   vertical: "top",
                   horizontal: "right",
+                }}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "& .MuiAvatar-root": {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    "&::before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
+                  },
                 }}
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
